2b81c106cc7e73ede1e070c05ec09a6015b4ca0c
Robert Carr <robert@rightscale.com>
2011-12-02 20:50:14 +0000

    Added shell configuration recipe

--- cookbooks/cde/recipes/config_git.rb
+++ cookbooks/cde/recipes/config_git.rb
@@ -1,3 +1,10 @@
+# Install the basic git package and configuration template to
+# 
+
+package "git" do
+  action :install
+end
+
 template "~/.gitconfig" do
   source "gitconfig.erb"
 end
---
+++ cookbooks/cde/recipes/config_shell.rb
@@ -0,0 +1,19 @@
+# Add customized prompt & autocomplete for shell
+#
+
+template "~/.git_complete.sh" do
+  source "git-completion.bash"
+end
+
+template "~/.vimrc" do
+  source "vimrc.erb"
+end
+
+bash "Activate git prompt" do
+  code <<-EOH
+  echo "source ~/.git_complete.sh" >> ~/.bashrc
+  echo export PS1='\u@\h:\w$(__git_ps1 " [\[\e[34;1m\]%s\[\e[0m\]]")\$ ' >> ~/.bashrc
+  echo export GIT_PS1_SHOWDIRTYSTATE=true >> ~/.bashrc
+  echo export GIT_PS1_SHOWUNTRACKEDFILES=true >> ~/.bashrc
+  EOH
+end
