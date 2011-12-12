b13ef05888c6d307861dd3ecc4da20dbe2bfcc67
root <root@ip-10-32-159-234.ec2.internal>
2011-12-12 22:24:22 +0000

    Updated metadata

--- cookbooks/cde/metadata.rb
+++ cookbooks/cde/metadata.rb
@@ -4,3 +4,10 @@ license          "All rights reserved"
 description      "Installs/Configures cde"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+
+recipe "cde::config_shell", "Configures a basic shell environment"
+recipe "cde::config_git", "Configures basic git environment"
+recipe "cde::config_knife", "Configures basic knife environment"
+
+
