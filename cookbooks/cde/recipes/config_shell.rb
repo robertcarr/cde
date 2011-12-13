96851f0ee0a192026be619f019d68dc9af465f0a
Robert Carr <robert@rightscale.com>
2011-12-13 13:33:43 -0800

    test

--- cookbooks/cde/recipes/config_shell.rb
+++ cookbooks/cde/recipes/config_shell.rb
@@ -16,7 +16,7 @@ end
 bash "Activate environment" do
   code <<-EOH
   echo "source /root/.git_completion.sh" >> ~/.bashrc
-  echo "export PS1='\u@\h:\w$\(__git_ps1 \" [\\[\\e[34;1m\\]%s\\[\\e[0m\\]]\"\)\$ '" >> /root/.bashrc
+  echo "export PS1='\\u@\\h:\\w$\\(__git_ps1 \\" [\\[\\e[34;1m\\]%s\\[\\e[0m\\]]\\"\\)\$ '" >> /root/.bashrc
   echo export GIT_PS1_SHOWDIRTYSTATE=true >> /root/.bashrc
   echo export GIT_PS1_SHOWUNTRACKEDFILES=true >> /root/.bashrc
   echo export PATH=$PATH:/opt/rightscale/sandbox/bin >> /root/.bashrc
